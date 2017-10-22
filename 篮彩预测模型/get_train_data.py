92f356ae8b1696f818c8cabf30895b8b4228b793
imsj <1094539225@qq.com>
2017-10-22 22:50:30 +0800

    训练数据生成-中间过程

--- 篮彩预测模型/get_train_data.py
+++ 篮彩预测模型/get_train_data.py
@@ -6,10 +6,17 @@ class GetTrainData:
         self.start_index = n
         self.last_N_game = n
         self.factors = ['FGM', 'FTM', 'AST', 'FG3M', 'OREB']
+        self.factors_opp = ['PTS_OPP', 'FGM_OPP', 'FG3M_OPP', 'AST_OPP', 'OREB_OPP', 'DREB']
         self.quantiles = [0.25, 0.75]
+
         self.big_suffix = '_big_0.75'
         self.small_suffix = '_small_0.25'
         self.features = [f + s for f in self.factors for s in [self.big_suffix, self.small_suffix]]
+
+        self.big_suffix_opp = '_big_mean'
+        self.small_suffix_opp = '_small_mean'
+        self.features_opp = [f + s for f in self.factors_opp for s in [self.big_suffix_opp, self.small_suffix_opp]]
+
         self.is_big_or_small_code = {'small': 100, 'big': 200, 'none': 300}
 
     def is_big_or_small(self, series):
@@ -35,9 +42,9 @@ class GetTrainData:
     def get_team_features(self, df_team):
         df_factor = df_team[self.factors]
         df_result = pd.DataFrame()
-        for i in range(self.start_index, df_team.shape[0]):
-            game_series = self.get_game_feature(df_factor.iloc[i - self.last_N_game:i].quantile(self.quantiles),
-                                                df_factor.iloc[i])
+        for i in range(self.start_index, df_factor.shape[0]):
+            df_base = df_factor.iloc[i - self.last_N_game:i].quantile(self.quantiles)
+            game_series = self.get_game_feature(df_base, df_factor.iloc[i])
             df_result = df_result.append(game_series, ignore_index=True)
         return df_result
 
@@ -46,3 +53,5 @@ class GetTrainData:
         df_factor = df_team[self.factors]
         return (df_factor.iloc[i - self.last_N_game:i].quantile(self.quantiles),
                 df_factor.iloc[i])
+
+    def get_gamex_feature(self, df_up_to_date, team, opp, opp_next):
